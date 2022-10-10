2c35d87d4e8c7536c3a2e3ecfaf6da16dd7b5e02
huai0216 <t108820013@ntut.org.tw>
2022-10-10 16:44:34 +0800

    make dropdown list and show pic

--- week4/menu.py
+++ week4/menu.py
@@ -1,72 +1,25 @@
-"""
-Created on Fri Oct  7 14:39:38 2022
+#建立元件
+button_red = widgets.Button(description="紅色藥丸")
+button_blue = widgets.Button(description="藍色藥丸")
+out = widgets.Output()
 
-@author: huai
+#定義函式
+def on_button_red_clicked(_):
+      with out:
+          clear_output()
+          print("領你去看真相！")
+def on_button_blue_clicked(_):
+      with out:
+          clear_output()
+          print("歡迎來到安逸的虛擬世界～")
+            
+#設定按鈕被按下後會觸發的函式
+button_red.on_click(on_button_red_clicked)
+button_blue.on_click(on_button_blue_clicked)
 
-"""
-import ipywidgets as widgets
-from IPython.display import display, clear_output
-
-meal_dic = {
-    '無':0,
-    '牛肉麵\t100元':100,
-    '肉燥飯\t50元':50,
-    '餛飩麵\t70元':70,
-    }
-dessert_dic = {
-    '無':0,
-    '薯餅\t25元':25,
-    '熱狗\t20元':20,
-    '雞塊\t30元':30,
-    }
-drink_dic ={
-    '無':0,
-    '紅茶\t20元':20,
-    '檸檬紅茶\t25元':25,
-    '奶茶\t30元':30,
-    }
-
-meal = widgets.Dropdown(
-    options = meal_dic.keys(),
-    value = '無',
-    description = "主食: ",
-    disabled = False,
-)
-
-dessert = widgets.Dropdown(
-    options = dessert_dic.keys(),
-    value = "無",
-    description = "點心: ",
-    disabled = False,
-)
-drink = widgets.Dropdown(
-    options = drink_dic.keys(),
-    value = '無',
-    description = "飲料",
-    disabled = False,
-)
-
-amount_meal = widgets.Text(
-    value='0',
-    placeholder='請輸入數量',
-    description='數量: ',
-    disabled=False
-)
-amount_dessert = widgets.Text(
-    value = "0",
-    placeholder = "請輸入數量",
-    description = "數量: ",
-    disabled = False
-)
-amount_drink = widgets.Text(
-    value = "0",
-    placeholder = "請輸入數量",
-    description = "數量: ",
-    disabled = False
-)
-order_meal = widgets.HBox([meal, amount_meal])
-order_dessert = widgets.HBox([dessert, amount_dessert])
-order_drink = widgets.HBox([drink, amount_drink])
-display(order_meal)
-display(order_dessert)
-display(order_drink)
+#排版並輸出
+#results = widgets.HBox([button_red,button_blue,out])
+#results
+display(button_red)
+display(button_blue)
+display(out)
